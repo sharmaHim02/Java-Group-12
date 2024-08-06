7eccbf0244425e1d2ebcca60accc0aae56e6d94f
Ritik kumar <ritikkumar95883@gmail.com>
2024-08-06 01:23:49 +0100

    Created a new branch for consumer

--- src/main/java/com/mycompany/foodwastereductionplatform/JakartaRestConfiguration.java
+++ src/main/java/com/mycompany/foodwastereductionplatform/JakartaRestConfiguration.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.Application;
 /**
  * Configures Jakarta RESTful Web Services for the application.
  * @author Juneau
- */
+ */            
 @ApplicationPath("resources")
 public class JakartaRestConfiguration extends Application {
     
